c6b3531f65ea638d413fe5048da3b6c4e5211f96
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-19 22:59:00 -0500

    commit tree c0612f5bf60616b1f8b1e88ddd5f90eec304a3f4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length > 3
+        array.last.prepend("and ")
+        return fruit = array.join(", ")
+    elsif array.length == 3
+        array.last.prepend("and ")
+        return fruit = array.join(", ")
+    elsif array.length == 2
+        array.last.prepend("and ")
+        return fruit = array.join(" ")
+    else return array.join
+    end
 end
+
